5b9ff0243922574cce93c9e58dae4742440ead75
Adam J Gray <adam.j.gray@gmail.com>
2015-09-03 13:22:18 -0400

    Working version

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
 app.get('/', function(req, res) {
-  res.redirect(302, req.params.url);
+  res.redirect(302, req.query.url);
+});
+
+var server = app.listen(app.get('port'), function() {
+  console.log('Node is running on port', app.get('port'))
 });
